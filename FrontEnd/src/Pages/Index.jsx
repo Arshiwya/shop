79da8c648e4254dda258ba0134f0f94dccdbcd12
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-04-03 15:28:35 +0330

    add SliderIndex

--- FrontEnd/src/Pages/Index.jsx
+++ FrontEnd/src/Pages/Index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Header/Navbar'
+import SliderIndex from '../Components/Slider/SliderIndex'
 
 export default function Index() {
   return (
     <>
     <Navbar/>
+    <div className="container">
+    <SliderIndex/>
+    </div>
     </>
   )
 }
