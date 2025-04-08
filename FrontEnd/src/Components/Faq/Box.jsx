76ffc10295e53ad13e1b85b24e39ad1a7685f5d5
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-04-08 16:52:17 +0330

    cerate Box

---
+++ FrontEnd/src/Components/Faq/Box.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export default function Box({ title, question, answer }) {
+  const [isShowDesc, setIsShowDesc] = useState(false);
+
+  return (
+    <>
+      <div className="">
+        <span className="font-DanaMeduim text-lg "> {title}</span>
+        <div>
+          <div className="flex justify-between mt-8">
+            <h1 className="pb-7 font-Dana text-lg">
+              {question}
+            </h1>
+            <svg
+              onClick={() => setIsShowDesc((prev) => !prev)}
+              className={
+                isShowDesc
+                  ? "w-4 h-4 rotate-180 transition-all ml-12"
+                  : "w-4 h-4 transition-all ml-12"
+              }
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+          {isShowDesc ? (
+            <div>
+              <span className="font-Dana w-7 leading-8 ">
+               {answer}
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
--- FrontEnd/src/Components/Faq/Faq.jsx
+++ FrontEnd/src/Components/Faq/Faq.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../Header/Navbar";
 import Footer from "../Footer/Footer";
 import Button from "./Button";
+import Box from "./Box";
 
 export default function Faq() {
-  const [isShowDesc, setIsShowDesc] = useState(false);
-
   return (
     <>
       <Navbar />
@@ -24,49 +23,16 @@ export default function Faq() {
           <Button title={"پرداخت و استرداد"} />
         </div>
 
-        <div className="">
-          <span className="font-DanaMeduim text-lg ">ضمانت خانومی</span>
-          <div>
-            <div className="flex justify-between mt-8">
-              <h1 className="pb-7 font-Dana text-lg">
-                آیا خانومی اصالت کالا را بررسی میکند؟
-              </h1>
-              <svg
-                onClick={() => setIsShowDesc((prev) => !prev)}
-                className={
-                  isShowDesc
-                    ? "w-4 h-4 rotate-180 transition-all ml-12"
-                    : "w-4 h-4 transition-all ml-12"
-                }
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            </div>
-            {isShowDesc ? (
-              <div>
-                <span className="font-Dana w-7 leading-8 mb-8">
-                  خانومی نمایندگی رسمی برندهای آرایشی بهداشتی و مراقبتی را
-                  داراست و تمامی محصولات را بدون واسطه، مستقیماً از تولیدکننده
-                  اصلی یا وارد کننده رسمی برند دریافت نموده و عرضه می‌نماید. به
-                  این ترتیب، اصالت کالا، قیمت نمایندگی و تاریخ انقضای درست محصول
-                  با اطمینان توسط خانومی تضمین می‌شود.
-                </span>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-
+        {/* Boxs */}
+        <Box
+          title={"ضمانت خانومی"}
+          question={"آیا خانومی اصالت کالاها را تضمین می‌کند؟"}
+          answer={
+            "خانومی نمایندگی رسمی برندهای آرایشی بهداشتی و مراقبتی را داراست و تمامی محصولات را بدون واسطه، مستقیماً از تولیدکننده اصلی یا وارد کننده رسمی برند دریافت نموده و عرضه می‌نماید. به این ترتیب، اصالت کالا، قیمت نمایندگی و تاریخ انقضای درست محصول با اطمینان توسط خانومی تضمین می‌شود."
+          }
+        />
+        <Box />
+        <Box />
       </div>
       <Footer />
     </>
